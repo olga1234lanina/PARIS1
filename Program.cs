c00d7d9d6ebc5f7be6ed3524d67e80ecb61045e3
Olga <olga1234lanina@yandex.ru>
2023-08-18 20:05:46 +0500

    ремонт

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Привет, Миша");
+﻿// Написать программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа 
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число >");
+if (number < 100 || number >= 10000)
+{
+    Console.WriteLine("Это некорректное число, попробуйте еще раз:");
+    return;
+
+}
+Console.WriteLine($"Введенное число {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра данного числа {secondRank}");
